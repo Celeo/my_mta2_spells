ff364a461b833569f994f6d081f2241e3b5c2984
Celeo <celeodor@gmail.com>
2019-04-12 17:44:05 -0700

    Showing withstand

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const Spell = (props) => {
         { props.arcanum } { props.level }: { props.name }
         { props.rating > 0 && <span className="spell-rating">{ '⭐'.repeat(props.rating) }</span> }
       </h4>
+      { props.withstand ? <p className="withstand-rating">Withstand: { props.withstand }</p> : '' }
       <p className="spell-body"><strong>{ props.short }</strong></p>
       {
         props.reaches.map(r => <SpellReach key={r.count + ':' + r.effect} count={r.count} effect={r.effect} />)
@@ -38,6 +39,7 @@ Spell.propTypes = {
   level: PropTypes.number,
   name: PropTypes.string,
   psf: PropTypes.string,
+  withstand: PropTypes.string,
   short: PropTypes.string,
   reaches: PropTypes.array,
   rating: PropTypes.number
@@ -54,6 +56,7 @@ const Index = (props) => {
             level={s.level}
             name={s.name}
             psf={s.psf}
+            withstand={s.withstand}
             short={s.short}
             reaches={s.reaches}
             rating={s.rating}
