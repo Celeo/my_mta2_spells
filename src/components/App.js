cfdc9c9c91b02936aa0eefade3ffd0a4c730562d
Celeo <celeodor@gmail.com>
2019-05-02 09:48:43 -0700

    Simplified router code

--- src/components/App.js
+++ src/components/App.js
@@ -6,42 +6,6 @@ import ReactMarkdown from 'react-markdown'
 
 import SpellListing from './SpellListing.js'
 
-const ViewSpellListing = (props) => {
-  return (
-    <div className="container-medium">
-      <SpellListing spellData={props.spellData} />
-    </div>
-  )
-}
-
-ViewSpellListing.propTypes = {
-  spellData: PropTypes.object.isRequired
-}
-
-const ViewCasting = (props) => {
-  return (
-    <div className="container-medium">
-      <ReactMarkdown source={props.castingRules} className="markdown" />
-    </div>
-  )
-}
-
-ViewCasting.propTypes = {
-  castingRules: PropTypes.string.isRequired
-}
-
-const ViewFateAndTime = (props) => {
-  return (
-    <div className="container-medium">
-      <ReactMarkdown source={props.fateAndTimeRules} className="markdown" />
-    </div>
-  )
-}
-
-ViewFateAndTime.propTypes = {
-  fateAndTimeRules: PropTypes.string.isRequired
-}
-
 const App = (props) => {
   const navBrand = <a href="/" className="brand">Mage Helper</a>
   return (
@@ -51,10 +15,12 @@ const App = (props) => {
         <NavLink to="/casting">Casting Rules</NavLink>
         <NavLink to="/fate_and_time" className="last-nav-item">Fate and Time Rules</NavLink>
       </Navbar>
-      <Route path="/" exact render={() => <Redirect to="/spells" />} />
-      <Route path="/spells" render={() => <ViewSpellListing spellData={props.spellData} />} />
-      <Route path="/casting" render={() => <ViewCasting castingRules={props.castingRules} />} />
-      <Route path="/fate_and_time" render={() => <ViewFateAndTime fateAndTimeRules={props.fateAndTimeRules} />} />
+      <div className="container-medium">
+        <Route path="/" exact render={() => <Redirect to="/spells" />} />
+        <Route path="/spells" render={() => <SpellListing spellData={props.spellData} />} />
+        <Route path="/casting" render={() => <ReactMarkdown source={props.castingRules} className="markdown" />} />
+        <Route path="/fate_and_time" render={() => <ReactMarkdown source={props.fateAndTimeRules} className="markdown" />} />
+      </div>
     </Router>
   )
 }
